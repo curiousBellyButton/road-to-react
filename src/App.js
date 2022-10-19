1f20a5f3238d2b4806743de9f2cd8ffe52f3e719
James H Nguyen <108825689+curiousBellyButton@users.noreply.github.com>
2022-10-19 19:36:58 +0700

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>   
+      <h1>Hello {getTitle('World')}</h1>   
+    
+    <label htmlFor="search">Search</label>
+    <input id="search" type="text" />
     </div>
   );
 }
 
-export default App;
+export default App
