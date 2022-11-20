990b8d2ef77b48813d74ffc950b139fc006b1762
James H Nguyen <108825689+curiousBellyButton@users.noreply.github.com>
2022-11-20 20:39:33 +0700

    handler/props/state/callback handlers

--- src/App.js
+++ src/App.js
@@ -1,65 +1,91 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// itemMap can be named anything
 
-// function getTitle(title) {
-//   return title;
-// }
-
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-];
-
-let List = () => 
- (
-    <ul>
-      {list.map(function(item) {
-        return (
-          <li key={item.objectID}>
-            <span> 
-              <a href={item.url}>{item.title}</a> 
-            </span>
-            <span> author: {item.author} </span>
-            <span>comments: {item.num_comments}</span>
-            <span>points: {item.points} </span>
-          </li>
-        );  
-      })}
-    </ul>
+let List = (props) => 
+ (  <>
+      <h1>{props.headerTitle}</h1>
+      <ul>
+        {props.listStories.map(function(itemMap) {
+          return (<Item item={itemMap} headerTitle = "headerTitleItem" />);  
+        })}
+      </ul>
+    </>
   )
 
+const Item = (props) => {
+  return (
+    <li key={props.item.objectID}>
+      <span> 
+        <a href={props.item.url}>{props.item.title }</a> 
+      </span>
+      <span>author: {props.item.author} </span>
+      <span>comments: {props.item.num_comments}</span>
+      <span>points: {props.item.points} </span>
+      <span>reviews: {props.item.reviews} </span>
+      <h2>{props.headerTitle}</h2>
+    </li>
+  )
+} 
 
+function Search(props) {
+  
+  const [searchTerm, setSearchTerm] = React.useState('');
 
-function Search() {
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+  }
+ 
+  const handleMouseOver = (event) => {
+    console.log(event);
+  }
   return (
     <>
       <label htmlFor="search">Search</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} onMouseOver={handleMouseOver} />
+    <p>
+      Searching for <strong>{searchTerm}</strong>
+    </p>
     </>
   )
 }
+
 function App() {
+
+  const stories = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    reviews: 333,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    reviews: 246,
+    objectID: 1,
+    },
+  ];
+
+  const handleSearch = (event)  => {
+    console.log(event.target.value);
+  }
+  // listStories can be named anything, in the props component, it must be the same, e.g. props.listStories.title
+
   return (
     <div>
       <h1>My Hacker Stories</h1>   
-      <Search />     
+      <Search onSearch={handleSearch} />     
       <hr />
-      <List />
+      <List listStories={stories} headerTitle = "headerTitleList" />
     </div>
   );
 }
--- src/Math.js
+++ src/Math.js
@@ -12,6 +12,14 @@ let sqaureRoot2 = function (number) {
 const squareRoot3 = number =>  number * number;
 
 
-console.log(sqaureRoot(3));
+console.log(sqaureRoot(2));
 console.log(sqaureRoot2(3));
-console.log(squareRoot3(3));
+console.log(squareRoot3(4));
+
+function testFirstMessage () {
+  console.log("first message")
+}
+
+setTimeout(testFirstMessage, 5000);
+
+
